efd43235a703792892940c45b7cdbb657ddecf90
Josh H <josh@niveous.co>
2019-03-13 22:03:45 -0400

    Initial commit

---
+++ Application.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @brief		VarDumper Application Class
+ * @author		<a href='https://dev.taiga.sh/ips/var-dumper'>Makoto</a>
+ * @copyright	(c) 2019 Makoto
+ * @package		Invision Community
+ * @subpackage	VarDumper
+ * @since		14 Mar 2019
+ * @version		
+ */
+ 
+namespace IPS\vardumper;
+
+/**
+ * VarDumper Application Class
+ */
+class _Application extends \IPS\Application
+{
+	
+}
